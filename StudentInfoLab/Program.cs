6ca67e48a0d15736a98647bf777b34485a7224e3
Tammy <tammymarie.cole@gmail.com>
2017-10-02 22:33:14 -0400

    first commit

---
+++ StudentInfoLab/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentInfoLab
+{
+    class Program
+    {
+        private static object Student;
+
+        static void Main(string[] args)
+        {
+            {
+                //create list of students, student numbers, hometown and favorite food
+                Student Molly = new Student("Molly", 1, "Rockford", "Noodles");
+                Student Michelle = new Student("Michelle", 2, "Ecuador", "Spaghetti");
+                Student Rebecca = new Student("Rebecca", 3, "Charlevoix", "Pizza");
+                Student Brianna = new Student("Brianna", 4, "Fairfield", "Gumbo");
+                Student Tammy = new Student("Tammy", 5, "Grand Rapids", "Maru Sushi");
+                Student Lauren = new Student("Lauren", 6, "Plainwell", "Cheeseburgers");
+                Student Karina = new Student("Karina", 7, "Holland", "Tacos");
+                Student Anel = new Student("Anel", 8, "Grand Rapids", "Pizza");
+                Student Lisa = new Student("Lisa", 9, "Los Angeles", "Mushroom Masala Dosa");
+                Student Jonaca = new Student("Jonaca", 10, "Lansing", "Sushi");
+                Student Tanvi = new Student("Tanvi", 11, "India", "Chicken Biryani");
+
+                List<Student> students = new List<Student>();
+                students.Add(Molly);
+                students.Add(Michelle);
+                students.Add(Rebecca);
+                students.Add(Brianna);
+                students.Add(Tammy);
+                students.Add(Lauren);
+                students.Add(Karina);
+                students.Add(Anel);
+                students.Add(Lisa);
+                students.Add(Jonaca);
+                students.Add(Tanvi);
+
+                Console.WriteLine("Welcome to our C# class.");
+
+                bool run = true;
+                while (run == true)
+                {
+
+                    //prompt the user to ask about a particular student
+                    Console.WriteLine("Which student would you like to learn more about? (enter a number 1-11 ).");
+                    int userRequest = Int32.Parse( Console.ReadLine());
+
+                    Student requested = students[userRequest-1];
+                    Console.WriteLine("Student " + userRequest + " is " + requested.GetStudentName());
+
+                    Console.WriteLine("What would you like to know about " + requested.GetStudentName() + " ?");
+                    Console.WriteLine("Choose hometown or favorite food");
+                    string choice = Console.ReadLine();
+
+                    //Return the proper responses according to user-submitted information
+                    if (choice == "hometown")
+                    {
+                        requested.GetHometown();
+                    }
+                    else if (choice == "favorite food") 
+                    {
+                        requested.GetFavoriteFood();
+                    }
+                    run = Continue();
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+            public static bool Continue()
+        {
+            Console.WriteLine("Would you like to learn about another student? y/n");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "y")
+            {
+                goOn = true;
+            }
+            else if (input == "n")
+            {
+                goOn = false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = Continue();
+            }
+
+            return goOn;
+        }
+    }
+}
---
+++ StudentInfoLab/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoLab
+{
+    //This class represents the blueprint for building us a car 
+    class Student
+    {
+        //Note that 
+        private string name
+        { get; set; }
+        private int studentNumber
+        { get; set; }
+        private string hometown
+        { get; set; }
+        private string favoriteFood
+        { get; set; }
+        public static string requested { get; internal set; }
+
+        //this is a special method called the constructor  
+        //Whenever we want to build an object we call this method
+        //The car constructor builds and then returns a car object
+        public Student(String n, int i, String hT, String fF)
+        {
+            name = n;
+            studentNumber = i;
+            hometown = hT;
+            favoriteFood = fF;
+
+            Console.WriteLine(name + " has been created!");
+        }
+        
+        public string GetStudentName()
+        {
+            return name;
+        }
+
+        public int GetStudentNumber()
+        {
+            return studentNumber;
+        }
+
+        public void GetHometown()
+        {
+            Console.WriteLine(name+"'s hometown is " + hometown);
+        }
+
+        public void GetFavoriteFood()
+        {
+            Console.WriteLine(name + "'s favorite food is " + favoriteFood);
+        }
+    }
+}
